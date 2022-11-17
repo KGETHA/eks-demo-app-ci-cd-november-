76edf2464a7cc5e221f17bfaf34cc0c485ac6709
KGETHA <111444703+KGETHA@users.noreply.github.com>
2022-11-17 16:51:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! - welcome to 4icu program')
+  res.send('Hello World! - welcome to 5icu program')
 })
 
 app.listen(port, () => {
